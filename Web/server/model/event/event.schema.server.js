99d0daa569f15abe962c18bac11a9d7628e0abd3
Rakesh Ravi Shankar <Rakesh@Rakeshs-MacBook-Pro.local>
2017-04-08 22:26:46 -0400

    Event Schema

--- Web/server/model/event/event.schema.server.js
+++ Web/server/model/event/event.schema.server.js
@@ -1,3 +1,22 @@
 /**
  * Created by rohansapre on 4/8/17.
  */
+var mongoose = require("mongoose");
+
+var eventSchema = mongoose.Schema({
+    creatorID: String,
+    eventID: String,
+    initialPicURL: String,
+    endPicURL: String,
+    date: Date,
+    time: String,
+    purpose: String,
+    eventName: String,
+    numParticipants: Number,
+    closed: Boolean,
+    // eventTags: [String],
+    location: String,
+    dateCreated: {type: Date, default: Date.now()}
+}, {collection: event.collection});
+
+module.exports = eventSchema;
