047d5d8ee59210413e880cdefe9bb924fdb36557
vaibhavshukla <vaibhavshukla@172-30-30-161.DYNAPOOL.NYU.EDU>
2017-04-08 22:25:44 -0400

    Modified changes to the fragment event list

--- Android/CleanHood/app/src/main/java/cleanhood/ny/hack/edu/cleanhood/fragments/EventListFragment.java
+++ Android/CleanHood/app/src/main/java/cleanhood/ny/hack/edu/cleanhood/fragments/EventListFragment.java
@@ -3,15 +3,21 @@ package cleanhood.ny.hack.edu.cleanhood.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import cleanhood.ny.hack.edu.cleanhood.R;
 
 /**
  * Created by vaibhavshukla on 4/8/17.
  */
 
 public class EventListFragment extends Fragment {
+
+    private RecyclerView mEventListView;
+    private View rootView;
 
 
     @Override
@@ -22,6 +28,7 @@ public class EventListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        rootView = inflater.inflate(R.layout.event_list_fragment, container, false);
+        return rootView;
     }
 }
