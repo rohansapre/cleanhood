3b9ce7d80717d1dbb456a314dd43b5934af3dca1
Bharat V <vaidhyanathan.b@husky.neu.edu>
2017-04-08 23:14:47 -0400

    EventList singleton structure created

--- Android/CleanHood/app/src/main/java/cleanhood/ny/hack/edu/cleanhood/utilities/EventList.java
+++ Android/CleanHood/app/src/main/java/cleanhood/ny/hack/edu/cleanhood/utilities/EventList.java
@@ -1,10 +1,28 @@
 package cleanhood.ny.hack.edu.cleanhood.utilities;
 
+import java.util.ArrayList;
+
+import cleanhood.ny.hack.edu.cleanhood.valueObjects.Event;
+
 /**
  * Created by vaidhyanathannarayanan on 08/04/17.
  */
 
 public class EventList {
+
+    EventList instance = null;
+    ArrayList<Event> events;
+
+    private EventList(){
+        events = new ArrayList<Event>();
+    }
+
+    public EventList getInstance(){
+        if(instance == null){
+            instance = new EventList();
+        }
+        return instance;
+    }
 
     
 
