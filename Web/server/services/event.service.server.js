1342ea76e5537ef684981a3b661489a5c8b13907
Amritansh Tripathi <amritanshtripathi@gmail.com>
2017-04-08 22:26:45 -0400

    working on user.service

--- Web/server/services/event.service.server.js
+++ Web/server/services/event.service.server.js
@@ -1,6 +1,6 @@
 /**
  * Created by Amritansh on 4/8/2017.
  */
-module.exports =function(app, EventModel){
+module.exports =function(app, Model){
 
 };
--- Web/server/services/user.server.service.js
+++ Web/server/services/user.server.service.js
@@ -1,6 +1,25 @@
 /**
  * Created by Amritansh on 4/8/2017.
  */
-module.exports =function(app, UserModel){
+module.exports =function(app, Model){
 
+    function createUser(req, res) {
+        var user=req.body;
+        Model
+            .createUser(user)
+            .then(
+                function (user) {
+                    res.json(user);
+                },
+                function (error) {
+                    res.status(404).send(error);
+                }
+            )
+    };
+
+    function deleteUser(req, res) {
+        var UserId= req.params.userId;
+        model
+            .deleteUser()
+    }
 };
