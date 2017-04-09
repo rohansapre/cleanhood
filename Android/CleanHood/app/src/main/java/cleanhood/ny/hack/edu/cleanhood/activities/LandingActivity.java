76a63d727935de0feb5453c5cdec6c73db46864f
vaibhavshukla <vaibhavshukla@172-30-30-161.DYNAPOOL.NYU.EDU>
2017-04-08 22:13:42 -0400

    Created base architecture of activities and fragments

--- Android/CleanHood/app/src/main/java/cleanhood/ny/hack/edu/cleanhood/activities/LandingActivity.java
+++ Android/CleanHood/app/src/main/java/cleanhood/ny/hack/edu/cleanhood/activities/LandingActivity.java
@@ -1,15 +1,26 @@
-package cleanhood.ny.hack.edu.cleanhood;
+package cleanhood.ny.hack.edu.cleanhood.activities;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import cleanhood.ny.hack.edu.cleanhood.R;
+import cleanhood.ny.hack.edu.cleanhood.fragments.EventListFragment;
+import cleanhood.ny.hack.edu.cleanhood.utilities.Constants;
 
 public class LandingActivity extends AppCompatActivity {
+    private int mContainerId ;
+    private FragmentTransaction fragmentTransaction;
+    private FragmentManager fragmentManager =getFragmentManager();
+    private String mCurrentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,12 @@ public class LandingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_landing);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mContainerId = R.id.container_fragment;
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(mContainerId, new EventListFragment(), Constants.EVENT_LIST_FRAGMENT);
+        mCurrentFragment = Constants.EVENT_LIST_FRAGMENT;
+        fragmentTransaction.commitAllowingStateLoss();
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +43,21 @@ public class LandingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void replaceFragment(Fragment fragment, String tag) {
+
+        try {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(mContainerId, fragment, tag);
+            fragmentTransaction.addToBackStack(tag);
+            mCurrentFragment = tag;
+            fragmentTransaction.commitAllowingStateLoss();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
     }
 
     @Override
