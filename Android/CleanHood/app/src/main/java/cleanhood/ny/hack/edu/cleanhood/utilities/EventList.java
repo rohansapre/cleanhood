ffa94afe07131aebc9d487c266da1d85c1af104b
Bharat V <vaidhyanathan.b@husky.neu.edu>
2017-04-08 23:26:59 -0400

    Finished EventList singleton and its methods : needs review

--- Android/CleanHood/app/src/main/java/cleanhood/ny/hack/edu/cleanhood/utilities/EventList.java
+++ Android/CleanHood/app/src/main/java/cleanhood/ny/hack/edu/cleanhood/utilities/EventList.java
@@ -24,6 +24,42 @@ public class EventList {
         return instance;
     }
 
-    
+    public void setEvents(ArrayList<Event> updatedEvents){
+        events = updatedEvents;
+    }
+
+    public ArrayList<Event> getEvents(){
+        return events;
+    }
+
+    public ArrayList<Event> getUpcomingEvents(){
+        ArrayList<Event> upcomingEvents = new ArrayList<Event>();
+        for(Event e : events){
+            if(!e.getClosed()){
+                upcomingEvents.add(e);
+            }
+        }
+        return upcomingEvents;
+    }
+
+    public ArrayList<Event> getPastEvents(){
+        ArrayList<Event> pastEvents = new ArrayList<Event>();
+        for(Event e : events){
+            if(e.getClosed()){
+                pastEvents.add(e);
+            }
+        }
+        return pastEvents;
+    }
+
+    public ArrayList<Event> getMyHostedEvents(String creatorID){
+        ArrayList<Event> myHostedEvents = new ArrayList<Event>();
+        for(Event e : events){
+            if(e.getCreatorID().equals(creatorID)){
+                myHostedEvents.add(e);
+            }
+        }
+        return myHostedEvents;
+    }
 
 }
